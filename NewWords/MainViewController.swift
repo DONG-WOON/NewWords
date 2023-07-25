17eba3cc0fe1653cf627d8ed35e96c30ffb173dd
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-07-25 16:39:43 +0900

    [refactor] change dictionary to enum
    신조어모음을 딕셔너리에서 열거형으로 변경

--- NewWords/MainViewController.swift
+++ NewWords/MainViewController.swift
@@ -14,16 +14,22 @@ class MainViewController: UIViewController {
     @IBOutlet weak var windowView: UIView!
     @IBOutlet var relatedWordButtons: [UIButton]!
     
-    let newWordsDictionary: [String: String] = [
-        "중꺽마": "중요한건 꺽이지 않는 마음",
-        "오운완": "오늘 운동 완료",
-        "그잡채": "그 자체를 재밌게 표현한 말",
-        "분좋카": "분위기 좋은 카페",
-        "완내스": "완전 내 스타일",
-        "킹받네": "열받네, 화나네, king + 열받네",
-        "별다줄": "별걸 다 줄인다",
-        "알잘딱깔센": "알아서 잘 딱 깔끔하고 센스있게"
-    ]
+    enum NewWord: String, CaseIterable {
+        case 중꺽마,오운완,그잡채,분좋카,완내스,킹받네,별다줄,알잘딱깔센
+        
+        var mean: String {
+            switch self {
+            case .중꺽마: return "중요한건 꺽이지 않는 마음"
+            case .오운완: return "오늘 운동 완료"
+            case .그잡채: return "그 자체를 재밌게 표현한 말"
+            case .분좋카: return "분위기 좋은 카페"
+            case .완내스: return "완전 내 스타일"
+            case .킹받네: return "열받네, 화나네, king + 열받네"
+            case .별다줄: return "별걸 다 줄인다"
+            case .알잘딱깔센: return "알아서 잘 딱 깔끔하고 센스있게"
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,8 +38,7 @@ class MainViewController: UIViewController {
         
         designSearchTextField()
         designWindowView()
-        
-//        setRandomRelatedWordButtons()
+    
         settingRandomWordButtons()
     }
     
@@ -51,67 +56,38 @@ class MainViewController: UIViewController {
             let alert = UIAlertController(title: "주의", message: "한글자 또는 빈칸으로는 입력이 불가능합니다.\n다시 입력해주세요.", preferredStyle: .alert)
             let ok = UIAlertAction(title: "확인", style: .default)
             let cancel = UIAlertAction(title: "취소", style: .cancel)
+            
             alert.addAction(ok)
             alert.addAction(cancel)
+            
             present(alert, animated: true)
             return
         }
         
-        let mean = getMeaning(of: sender.text)
+        let mean = NewWord(rawValue: text)?.mean
         resultLabel.text = wrapResultLabelText(mean)
-//        setRandomRelatedWordButtons()
         settingRandomWordButtons()
     }
     
     @IBAction func relatedWordButtonDidTapped(sender: UIButton) {
         searchTextField.text = sender.currentTitle
-        let mean = getMeaning(of: sender.currentTitle)
+        let mean = NewWord(rawValue: sender.currentTitle!)?.mean
         resultLabel.text = wrapResultLabelText(mean)
-//        setRandomRelatedWordButtons()
         settingRandomWordButtons()
     }
     
     func settingRandomWordButtons() {
-        let shuffledWords = newWordsDictionary.keys.shuffled()
+        let shuffledWords = NewWord.allCases.shuffled()
         for index in 0...relatedWordButtons.count - 1 {
-            relatedWordButtons[index].setTitle(shuffledWords[index], for: .normal)
+            relatedWordButtons[index].setTitle(shuffledWords[index].rawValue, for: .normal)
         }
     }
-    
-//    func getRandomWord(count: UInt = 1) -> [String] {
-//        let words = newWordsDictionary.keys
-//        var wordContainer = [String]()
-//
-//        switch count {
-//        case 0:
-//            let randomWord = words.randomElement()!
-//            wordContainer.append(randomWord)
-//            return wordContainer
-//        case 1...UInt(words.count):
-//            var shuffledWords = words.shuffled()
-//
-//            for _ in 1...count {
-//                let word = shuffledWords.popLast()!
-//                wordContainer.append(word)
-//            }
-//            return wordContainer
-//        default:
-//            return [""]
-//        }
-//    }
     
     private func mapTagWithRelatedWordButtons() {
         for index in 0...relatedWordButtons.count - 1 {
             relatedWordButtons[index].tag = index
         }
     }
-    
-//    private func setRandomRelatedWordButtons() {
-//        let words = getRandomWord(count: 3)
-//        for i in 0...2 {
-//            relatedWordButtons[i].setTitle(words[i], for: .normal)
-//        }
-//    }
     
     private func designSearchTextField() {
         searchTextField.layer.borderWidth = 1
@@ -126,14 +102,6 @@ class MainViewController: UIViewController {
         windowView.layer.shadowOpacity = 0.5
     }
     
-    private func getMeaning(of text: String?) -> String? {
-        if let text = text, let mean = newWordsDictionary[text] {
-            return mean
-        } else {
-            return nil
-        }
-    }
-    
     private func wrapResultLabelText(_ text: String?) -> String {
         if let text = text {
             return "\(searchTextField.text!)는 \(text)란 의미입니다."
@@ -142,6 +110,3 @@ class MainViewController: UIViewController {
         }
     }
 }
-
-
-
